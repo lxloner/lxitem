a805c120d5ff6ac178ab1efd79e6c0b70dd84da6
lxloner <x18234115020@163.com>
2019-03-13 23:38:35 +0800

    项目完成三分之二

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,23 @@
-//
-//		var oBox = $("#slideshow").children("p");
+//		var oBox = $(".image1").children();
 //		console.log(oBox);//获取ul中的li
-//		var oImgWidth = $(oBox.eq(0)).outerWidth();
+//		var oImgWidth = oBox.eq(0).outerWidth();
 //		console.log(oImgWidth);//获取每个li的宽度
-//		$("#slideshow").append('<p><img src="img/4644f0ab215f.jpg"/></p>');	
-//		console.log($('#slideshow'));
+//		
+//		
 //		var timer=null;
 //		var key = 0; //控制播放的数量
 //		var circle = 0;
-//		var olLis = $('#slideshow1');
+//		var olLis = $('.box li');
 //		timer = setInterval(autoPlay,1000);
 //		function autoPlay(){
 //			key ++;
 //			if(key > oBox.length){
-//				$("#slideshow").css({"left" : 0})
+//				$(".image1").css({"left" : 0})
 //				key = 1;
 //			}		
-//			$("#slideshow").animate({"left":-key * oImgWidth});			
+//			$(".image1").animate({"left":-key * oImgWidth});			
 // 			circle ++;
-// 			if(circle > olLis.length - 1){
+// 			if(circle > olLis.length){
 // 				circle = 0;
 // 			}
 // 			for(var i = 0,len = olLis.length;i < len;i ++){
@@ -31,9 +30,92 @@
 //				clearInterval(timer);
 //			})
 //			$(oBox[i]).mouseleave(function(){
-//				timer= setInterval(autoPlay,3000);;
+//				timer= setInterval(autoPlay,1000);;
 //			})
+//			
 //		}	 
 ////// (function(){
 //// 	
 //// })();
+(function(){
+            var $box = $(".slideshow"),
+                $picLi = $(".slideshow .image1 img"),
+                $tabLi = $(".slideshow .box li"),
+                len = $tabLi.length,
+                timer;
+                first = 0;
+            //初始化设置
+            $picLi.hide().eq(0).show();//隐藏$picLi 然后显示第一个
+            $tabLi.eq(0).addClass("on");//为第一张图片添加属性
+            $tabLi.eq(0).css("background","#fff");
+            //tab区域
+            $tabLi.click(function () {
+                var x = $(this).index();//tabLi[i].index = i;
+                if(x != first){
+                     change(x);//改变X的值
+                }
+            });
+            //自动轮播
+            auto();
+            $box.hover(function () {
+                clearInterval(timer);
+            },auto); 
+            function auto() {
+               timer = setInterval(function () {
+                    var x = first;
+                    x++;
+                    x %= len;
+                    change(x);
+                },4000)
+            }
+            //变化函数
+            function change(i) {
+                $picLi.eq(first).fadeOut(300);
+                $tabLi.eq(first).css("background","transparent");
+                $tabLi.eq(first).removeClass("on");
+                first = i;
+                $picLi.eq(first).fadeIn(300);
+                $tabLi.eq(first).css("background","#fff");
+                $tabLi.eq(first).addClass("on");
+            }
+        })();
+
+(function(){
+	var $box = $('.parts1'),
+		$bex = $('.parts1 .parts1-1 ul'),
+		$bsx = $('.parts1 ol li'),
+		first = 0;
+//	$bsx.hide().eq(0).show();
+	$bsx.eq(0).addClass("on");
+	$bsx.mousemove(function(){
+		var x = $(this).index();
+		if(x != first){
+			change(x);
+		}
+	});
+	function change(i){
+		$bex.eq(first).fadeOut(100);
+		$bsx.eq(first).removeClass("on");
+		first = i;
+		$bex.eq(first).fadeIn(100);
+		$bsx.eq(first).addClass("on");
+	}
+})();
+	$('.aaa li').each(function(){
+		$(this).mouseenter(function(){
+		$('.aaa li').css({'border-bottom':'none','color':'#000'});
+		$(this).css({'border-bottom':'3px solid #fc6628','color':'#fc6628'});
+	})
+	})
+//(function(){
+//	var $s1 = $('.s1');
+//	console.log($s1);
+//	$s1.mouseenter(function(){
+//		$s1.animate({top : 200});
+//		$s1.addClass("shadow");
+//	});
+//	$s1.mouseleave(function(){
+//		$s1.animate({top : 0});
+//		$s1.removeClass('shadow');
+//	})
+//})();
